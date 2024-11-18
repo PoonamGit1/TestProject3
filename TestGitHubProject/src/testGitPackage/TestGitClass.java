9750be7249a01ab0a112d2f393f1aa09f69eeca4
Poonam <Poonam@DESKTOP-LF031C5.lan>
2024-11-18 13:59:19 +0530

    Added Sysout1

--- TestGitHubProject/src/testGitPackage/TestGitClass.java
+++ TestGitHubProject/src/testGitPackage/TestGitClass.java
@@ -3,8 +3,8 @@ package testGitPackage;
 public class TestGitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello git");
+		System.out.println("Git1");
 	}
 
 }
